f39920f4685f9be5e033ba08ed20547d065804fe
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-24 01:13:50 +0800

    implemented products as local memory store to simulate db connection, getProducts handler also added

--- GOWEB/main.go
+++ GOWEB/main.go
@@ -15,9 +15,15 @@ func main() {
 	}
 
 	e := echo.New()
+	// DATABASE CONNECTION
+	products := []map[int]string{{1: "phone"}, {2: "tv"}, {3: "laptop"}}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello world")
 	})	
+	e.GET("/products", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, products)
+	})
 
 	e.Logger.Print(fmt.Sprintf("Listening on port %s", port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
