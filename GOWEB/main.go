c3a1a5d047d0ca34bcc1d94e9134b7505ccde05c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-23 19:48:00 +0800

    init helloworld server

--- GOWEB/main.go
+++ GOWEB/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("hello world")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusAccepted, "hello world")
+	})
+
+	e.Start(":8080")
 }
