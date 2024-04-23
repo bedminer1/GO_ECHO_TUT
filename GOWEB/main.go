12c18d1c4aca27b282d414e0f375551ffa8bb120
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-24 01:10:05 +0800

    changed port to env variable

--- GOWEB/main.go
+++ GOWEB/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	port := os.Getenv("MY_APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello world")
 	})	
 
-	e.Logger.Print("Listening on port 8080")
-	e.Logger.Fatal(e.Start("localhost:8080"))
+	e.Logger.Print(fmt.Sprintf("Listening on port %s", port))
+	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
 }
