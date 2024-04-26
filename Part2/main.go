c28a4fbb883301c4f09cc561e58ba680127c45cb
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-27 01:44:44 +0800

    custom correlation id header middleware

--- Part2/main.go
+++ Part2/main.go
@@ -11,13 +11,18 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/random"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const (
+	CorrelationID = "X-Correlation-ID"
+)
+
 var (
-	c *mongo.Client
+	// c *mongo.Client
 	db *mongo.Database
 	col *mongo.Collection
 	cfg config.Properties
@@ -38,12 +43,28 @@ func init() {
 	col = db.Collection(cfg.ProductCollection)
 }
 
+func addCorrelationID(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id := c.Request().Header.Get(CorrelationID)
+		if id == "" {
+			// generate ID
+			id = random.String(12)
+		}
+		// set to existing ID
+		c.Request().Header.Set(CorrelationID, id)
+		c.Response().Header().Set(CorrelationID, id)
+		
+		return next(c)
+	}
+}
+
 func main() {
 	e := echo.New()
 	h := handlers.ProductHandler{Col: col}
 
 	// MIDDLEWARE
 	e.Pre(middleware.RemoveTrailingSlash())
+	e.Pre(addCorrelationID) // passes correlation ID, passing to microservices
 
 	// HANDLERS
 	e.POST("/products", h.CreateProducts, middleware.BodyLimit("1M")) // 1mb payload limit
