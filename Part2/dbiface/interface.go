d3479a3621b403b2b686ba4d0b446207dd6476ca
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-26 16:13:17 +0800

    file set and mongodb connection init

---
+++ Part2/dbiface/interface.go
@@ -0,0 +1,7 @@
+package dbiface
+
+type (
+	CollectionAPI interface {
+		
+	}
+)
--- Part2/main.go
+++ Part2/main.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/bedminer1/SampleEchoServer/config"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	c *mongo.Client
+	db *mongo.Database
+	col *mongo.Collection
+	cfg config.Properties
+)
+
+func init() {
+	if err := cleanenv.ReadEnv(cfg); err != nil {
+		log.Fatalf("Config cannot be read: %v", err)
+	}
+	connectURI := fmt.Sprintf("mongodb://%s%s", cfg.DBHost, cfg.DBPort)
+	mongo.Connect(context.Background(), options.Client().ApplyURI(connectURI))
+}
 
 func main() {
 	e := echo.New()
+
+	// HANDLERS
+	e.POST("/products", CreateProduct)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
